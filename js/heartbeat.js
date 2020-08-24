2f6d8d4b454f3bd9f9c513a37a4229a12c1b30e0
Sander Klock <680327+Qrious@users.noreply.github.com>
2020-08-24 19:56:40 +0200

    Fix heartbeat script and disable login

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -1,39 +1,53 @@
-jQuery( document ).on( 'heartbeat-tick', function ( event, data ) {
-    // If there is no readonly info available, do nothing
-    if ( ! data.pivvenit_wordpress_readonly_info ) {
-        return;
-    }
-    const { __ } = wp.i18n;
+jQuery(document).on('heartbeat-tick', function (event, data) {
+        // If there is no readonly info available, do nothing
+        if (!data.pivvenit_wordpress_readonly_info) {
+            return;
+        }
 
-    /* Determine in which phase we are.
-     * There are two phases: the grace period, and the actual readonly
-     * During the grace period, we only notify the user that they should stop editing (and new logins are disabled).
-     * During the readonly period, changes are actually blocked server side.
-     */
+        const {__} = wp.i18n;
+
+        /* Determine in which phase we are.
+         * There are two phases: the grace period, and the actual readonly
+         * During the grace period, we only notify the user that they should stop editing (and new logins are disabled).
+         * During the readonly period, changes are actually blocked server side.
+         */
 
 
-    const upgradeInProgress = __( 'This website is in readonly mode, any changes you make from now on, will be lost.', 'pivvenit-wordpress-readonly' );
-    const readonlyInfo = data.pivvenit_wordpress_readonly_info;
-    if (window.wp.data !== undefined) {
-        window.wp.data.dispatch( 'core/notices' ).createNotice(
-            'warning',
-            upgradeInProgress,
-            {
-                id: readonlyInfo.phase1_start,
-                isDismissible: false
+        const upgradeInProgress = __('This website is in readonly mode, any changes you make from now on, will be lost.', 'pivvenit-wordpress-readonly');
+        const readonlyInfo = data.pivvenit_wordpress_readonly_info;
+
+        if (data.pivvenit_wordpress_readonly_info.status == "disabled") {
+            if (jQuery(`#${readonlyInfo.id}`).length > 0) {
+                jQuery(`#${readonlyInfo.id}`).remove();
             }
-        );
-    }
+            if (window.wp.data !== undefined && window.wp.data.dispatch('core/notices')) {
+                window.wp.data.dispatch('core/notices').removeNotice(readonlyInfo.id)
+            }
+            location.reload();
+            return;
+        }
 
-    if (jQuery('#test').length > 0) {
-        return;
-    }
+        if (window.wp.data !== undefined && window.wp.data.dispatch('core/notices')) {
+            window.wp.data.dispatch('core/notices').createNotice(
+                'warning',
+                upgradeInProgress,
+                {
+                    id: readonlyInfo.id,
+                    isDismissible: false
+                }
+            );
+        }
 
-    const $insertAfter = jQuery( '.wrap h1, .wrap h2' ).first();
-    const $elem = jQuery(`<div id='test'></div>`)
-        .addClass('notice')
-        .addClass('notice-warning')
-        .addClass('is-dismissible')
-        .append("<p>Upgrade in progress</p>");
-    $elem.insertAfter($insertAfter);
-});
+        if (jQuery(`#${readonlyInfo.id}`).length > 0) {
+            return;
+        }
+
+        const $insertAfter = jQuery('.wrap h1, .wrap h2').first();
+        const $elem = jQuery(`<div id='${readonlyInfo.id}'></div>`)
+            .addClass('notice')
+            .addClass('notice-warning')
+            .addClass('is-dismissible')
+            .append(`<p>${upgradeInProgress}</p>`);
+        $elem.insertAfter($insertAfter);
+    }
+);
--- wordpress-readonly.php
+++ wordpress-readonly.php
@@ -12,9 +12,14 @@
 add_filter( 'heartbeat_received', function($response, $data) {
     $readonlyInfo = get_option('pivvenit_wordpress_readonly_info');
     if (!$readonlyInfo) {
-        return $response;
+        $readonlyInfo = new \stdClass();
+        $readonlyInfo->id = (new DateTime("now"))->getTimestamp();
+        $readonlyInfo->status = 'disabled';
+        update_option('pivvenit_wordpress_readonly_info', json_encode($readonlyInfo));
+    } else {
+        $readonlyInfo = json_decode($readonlyInfo);
     }
-    $response['pivvenit_wordpress_readonly_info'] = json_decode($readonlyInfo, false);
+    $response['pivvenit_wordpress_readonly_info'] = $readonlyInfo;
     return $response;
 }, 10, 2 );
 
@@ -23,29 +28,56 @@ add_filter('admin_enqueue_scripts', function() {
     wp_set_script_translations('pivvenit_wordpress_readonly_heartbeat', 'pivvenit-wordpress-readonly');
 });
 
+add_action( 'admin_notices', function() {
+    $readonlyInfo = get_option('pivvenit_wordpress_readonly_info');
+    if (!$readonlyInfo) {
+        return;
+    }
+    $readonlyInfo = json_decode($readonlyInfo);
+    if ($readonlyInfo->status == 'disabled') {
+        return;
+    }
+    $class = 'notice notice-warning';
+    $message = __( 'This website is in readonly mode, any changes you make from now on, will be lost.', 'pivvenit-wordpress-readonly' );
+
+    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+});
+
+add_filter( 'wp_authenticate_user', function($user) {
+    $readonlyInfo = get_option('pivvenit_wordpress_readonly_info');
+    if (!$readonlyInfo) {
+        return $user;
+    }
+    $readonlyInfo = json_decode($readonlyInfo);
+    if ($readonlyInfo->status == 'disabled') {
+        return;
+    }
+    return new WP_Error( 'readonly_disabled_auth', __( "Login is temporary disabled due to maintenance. Please try again in a few moments.", 'pivvenit-wordpress-readonly' ) );
+}, 30, 1);
+
+
 add_action('plugins_loaded', function() {
     $readonlyInfo = get_option('pivvenit_wordpress_readonly_info');
     if (!$readonlyInfo) {
         return;
     }
     $data = json_decode($readonlyInfo);
-    $now = new DateTime("now");
+    if ($data->status != "readonly") {
+        return;
+    }
+
     if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         return;
     }
-    if (isset($data->phase2_start) && $now > new DateTime("@{$data->phase2_start}ts")) {
-        if (wp_is_json_request()) {
-            wp_send_json("Readonly mode", 503);
-        } else {
-            wp_die("Readonly mode");
-        }
+    if ($GLOBALS['pagenow'] === 'wp-login.php' && ! empty( $_POST['wp-submit'] ) && $_POST['wp-submit'] === 'Log In' ) {
+        return; // Handle this using the above authenticate filter
     }
-
-});
-
-
-register_deactivation_hook(__FILE__, function() {
-    delete_option('pivvenit_wordpress_readonly_info');
+        // Disallow all post requests
+    if (wp_is_json_request()) {
+        wp_send_json("Readonly mode", 503);
+    } else {
+        wp_die("Readonly mode");
+    }
 });
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
@@ -58,19 +90,28 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
          */
 
         $softReadonlyPeriod = new DateTime("now");
-        $readonlyInfo->phase1_start = $softReadonlyPeriod->getTimestamp();
+        $readonlyInfo->status = 'prepare';
+        $readonlyInfo->id = $softReadonlyPeriod->getTimestamp();
         update_option('pivvenit_wordpress_readonly_info', json_encode($readonlyInfo));
         WP_CLI::success("[{$softReadonlyPeriod->format('Y-m-d H:i:s')}] System is going in readonly mode in 60 seconds");
         WP_CLI::log("Waiting 60 seconds....");
         sleep(60);
         $beginReadonlyPeriod = new DateTime("now");
-        $readonlyInfo->phase2_start = $beginReadonlyPeriod->getTimestamp();
+        $readonlyInfo->status = 'readonly';
         update_option('pivvenit_wordpress_readonly_info', json_encode($readonlyInfo));
         WP_CLI::success("Starting readonly period at {$beginReadonlyPeriod->format('Y-m-d H:i:s')}");
     } );
 
     WP_CLI::add_command( 'readonly disable', function() {
-        delete_option('pivvenit_wordpress_readonly_info');
+        $readonlyInfo = get_option('pivvenit_wordpress_readonly_info');
+        if (!$readonlyInfo) {
+            $readonlyInfo = new \stdClass();
+            $readonlyInfo->id = (new DateTime("now"))->getTimestamp();
+        } else {
+            $readonlyInfo = json_decode($readonlyInfo);
+        }
+        $readonlyInfo->status = 'disabled';
+        update_option('pivvenit_wordpress_readonly_info', json_encode($readonlyInfo));
         WP_CLI::success("Disabled readonly mode");
     });
 }
